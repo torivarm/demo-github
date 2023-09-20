883d9fcf2ae834452f674362c14e0acf62116194
Tor Ivar Melling <tor.i.melling@ntnu.no>
2023-09-20 10:07:52 +0200

    web site

---
+++ web/main.tf
@@ -0,0 +1,46 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "random_string" "random_string" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+# Create Resource Group
+resource "azurerm_resource_group" "rg_web" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+# Create Storage Account
+resource "azurerm_storage_account" "sa_web" {
+  name                     = "staticweb${random_string.random_string.result}"
+  resource_group_name      = azurerm_resource_group.rg_web.name
+  location                 = azurerm_resource_group.rg_web.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  StaticWebsite {
+    index_document = "index.html"
+  }
+}
+
+# Add a index.html file to the storage account
+resource "azurerm_storage_blob" "index_html" {
+  name                   = "index.html"
+  storage_account_name   = azurerm_storage_account.sa_web.name
+  storage_container_name = azurerm_storage_container.sc_web.name
+  type                   = "Block"
+  content_type           = "text/html"
+  source                 = "<h1>Static web page deployed using Terraform</h1>"
+}
+
+
+resource "azurerm_storage_container" "sc_web" {
+  name                  = "$web"
+  storage_account_name  = azurerm_storage_account.sa_web.name
+  container_access_type = "private"
+}
+
---
+++ web/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.73.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg-backend-tfstate-dev"
+    storage_account_name = "sabetfss61r3a4sy5"
+    container_name       = "tfstate"
+    key                  = "dev-backend.terraform.tfstate"
+  }
+}
